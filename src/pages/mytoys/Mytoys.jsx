43243a5ea3e7a2a2766efdfac3a708181ea4b77c
Tapas chowdhury <tapaschy12@gmail.com>
2023-05-20 23:10:10 +0600

    ascending done

--- src/pages/mytoys/Mytoys.jsx
+++ src/pages/mytoys/Mytoys.jsx
@@ -3,50 +3,76 @@ import { UserToContext } from '../../provider/UserContext';
 import Singletoy from './Singletoy';
 
 const Mytoys = () => {
-    const [mytoys,settoys]=useState([]);
-    const {user}=useContext(UserToContext);
+    const [mytoys, settoys] = useState([]);
+    const { user } = useContext(UserToContext);
     console.log(user?.email)
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch(`http://localhost:5000/mytoys/${user?.email}`)
-        .then(res => res.json())
-        .then(data=>{
-            console.log(data);
-            settoys(data);
-        })
-    },[user])
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                settoys(data);
+            })
+    }, [user])
+
+    // sort
+    const [sortOrder, setSortOrder] = useState('desc');
+
+    useEffect(() => {
+
+        fetch(`http://localhost:5000/mytoys/${user?.email}/data?sortOrder=${sortOrder}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                settoys(data);
+            })
+    }, [sortOrder]);
+
+    const handleSortOrderChange = () => {
+        setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    };
 
 
     return (
-        <div className="overflow-x-auto w-full container mx-auto">
-            <table className="table w-full">
-                {/* head */}
-                <thead>
-                    <tr>
-                        <th>Toy</th>
-                        <th>Seller</th>
-                        <th>Sub-category</th>
-                        <th>Price</th>
-                        <th>Rating</th>
-                        <th>Edit</th>
-                        <th>Delete</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        mytoys.map(toy=>(<Singletoy
-                        key={toy._id}
-                        toy={toy}
-                        ></Singletoy>))
-                    }
 
-                </tbody>
-                {/* foot */}
-                <tfoot>
-                </tfoot>
+        <div>
+            <div>
+                Sort Order:
+                <button onClick={handleSortOrderChange}>
+                    {sortOrder === 'asc' ? 'Ascending' : 'Descending'}
+                </button>
+            </div>
+            <div className="overflow-x-auto w-full container mx-auto">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Toy</th>
+                            <th>Seller</th>
+                            <th>Sub-category</th>
+                            <th>Price</th>
+                            <th>Rating</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            mytoys.map(toy => (<Singletoy
+                                key={toy._id}
+                                toy={toy}
+                            ></Singletoy>))
+                        }
 
-            </table>
+                    </tbody>
+                    {/* foot */}
+                    <tfoot>
+                    </tfoot>
+
+                </table>
+            </div>
         </div>
     );
 };
